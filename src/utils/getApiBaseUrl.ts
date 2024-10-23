8ee868f5a9f11b461ee1261c007f87517d3f6b86
Arnold Szabo <szaboaarnold@gmail.com>
2024-10-23 11:40:22 +0200

    Fix android crash + fix base urls

--- src/utils/getApiBaseUrl.ts
+++ src/utils/getApiBaseUrl.ts
@@ -3,13 +3,13 @@ import { Environment } from "@dicetechnology/react-native-vesper-sdk";
 export const getApiBaseUrl = (environment: string): string => {
     switch (environment) {
         case Environment.DEVELOPMENT: {
-            return `https://dce-frontoffice-dev.imggaming.com/v2`;
+            return `https://dce-frontoffice-dev.imggaming.com/api/v2`;
         }
         case Environment.STAGING: {
-            return `https://dce-frontoffice-stag.imggaming.com/v2`;
+            return `https://dce-frontoffice-stag.imggaming.com/api/v2`;
         }
         default: {
-            return `https://dce-frontoffice.imggaming.com/v2`;
+            return `https://dce-frontoffice.imggaming.com/api/v2`;
         }
     }
 };
