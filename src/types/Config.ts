ee2411758db1ffaa085eb770f9d602c0fca566fb
grabofus <34451173+grabofus@users.noreply.github.com>
2024-10-25 12:36:18 +0100

    added guest login flow if username and password is empty

--- src/types/Config.ts
+++ src/types/Config.ts
@@ -1,9 +1,9 @@
 import { Environment } from "@dicetechnology/react-native-vesper-sdk";
 
 export interface Config {
-    PUBLIC_USERNAME: string;
-    PUBLIC_PASSWORD: string;
-    PUBLIC_REALM: string;
-    PUBLIC_API_KEY: string;
-    PUBLIC_ENV: Environment;
+    USERNAME: string;
+    PASSWORD: string;
+    REALM: string;
+    API_KEY: string;
+    ENV: Environment;
 }
