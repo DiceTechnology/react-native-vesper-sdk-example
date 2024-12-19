e8665ccf7235f674fbd007dd99a4bd029726cde1
Arnold Szabo <szaboaarnold@gmail.com>
2024-12-19 12:06:28 +0100

    Bump react-native-vesper-sdk to 0.4.0

--- src/utils/DemoAuthManager.ts
+++ src/utils/DemoAuthManager.ts
@@ -118,8 +118,10 @@ class DemoAuthManager implements AuthManager {
         const json = await response.json();
 
         this._authToken = json.authorisationToken;
-        this._refreshToken = json.refreshToken
-        
+        if (json.refreshToken) {
+            this._refreshToken = json.refreshToken;
+        }
+
         if (!this._authToken) {
             console.error('Invalid API response!');
             throw new Error('Invalid API response!');
